4c053b1f984e83c56919c8ea3e1084e2b1678abd
JamesonPhong <000882371@mohawkcollege.ca>
2026-09-23 03:19:32 -0400

    Add Models & some SFX
    - Added in the Models.
    - Implemented some of the SFX.

--- Team 8 DC1RP1 Clump Warriors/Assets/Scripts/PlayerCombat.cs
+++ Team 8 DC1RP1 Clump Warriors/Assets/Scripts/PlayerCombat.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     private Animator swordAnim;
+    public AudioSource swordSwing; // Audio source for sword swing sound
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +20,7 @@ public class PlayerCombat : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             swordAnim.SetTrigger("Slicing");
+            swordSwing.Play(); // Play the sword swing sound
         }
     }
 }
